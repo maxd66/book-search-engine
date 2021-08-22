a0b48d093d9242692ed2a99fba647c1243e54d9e
maxd66 <max.dunn63@gmail.com>
2021-08-21 23:57:06 -0700

    set up initial state for server to use apollo

--- server/server.js
+++ server/server.js
@@ -1,20 +1,35 @@
 const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
-const db = require("./config/connection");
+// TODO: REMOVE THIS PATH
 const routes = require("./routes");
+// ^^^^^^^^^^^^^^^^^^^^^^
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+// TODO REMOVE THIS USE
 app.use(routes);
+//^^^^^^^^^^^^^^^^^^^^
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
